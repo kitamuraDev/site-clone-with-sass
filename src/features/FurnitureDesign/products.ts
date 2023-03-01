7d77f0cc7dda2d7b1b790096b333a5deea62c5e3
kitamuraDev <masato1999824@icloud.com>
2023-03-01 22:48:46 +0900

    :sparkles: 商品一覧を作成

---
+++ src/features/FurnitureDesign/products.ts
@@ -0,0 +1,124 @@
+export type Products = {
+  id: number;
+  url: string;
+  imagePath: string;
+  title: string;
+  price: string;
+}[];
+
+const products: Products = [
+  {
+    id: 1,
+    url: '/furniture-design/products/item01',
+    imagePath: '/FurnitureDesign/item1.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 2,
+    url: '/furniture-design/products/item02',
+    imagePath: '/FurnitureDesign/item2.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 3,
+    url: '/furniture-design/products/item03',
+    imagePath: '/FurnitureDesign/item3.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 4,
+    url: '/furniture-design/products/item04',
+    imagePath: '/FurnitureDesign/item4.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 5,
+    url: '/furniture-design/products/item05',
+    imagePath: '/FurnitureDesign/item5.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 6,
+    url: '/furniture-design/products/item06',
+    imagePath: '/FurnitureDesign/item6.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 7,
+    url: '/furniture-design/products/item07',
+    imagePath: '/FurnitureDesign/item7.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 8,
+    url: '/furniture-design/products/item08',
+    imagePath: '/FurnitureDesign/item8.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 9,
+    url: '/furniture-design/products/item09',
+    imagePath: '/FurnitureDesign/item9.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 10,
+    url: '/furniture-design/products/item10',
+    imagePath: '/FurnitureDesign/item10.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 11,
+    url: '/furniture-design/products/item11',
+    imagePath: '/FurnitureDesign/item11.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 12,
+    url: '/furniture-design/products/item12',
+    imagePath: '/FurnitureDesign/item12.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 13,
+    url: '/furniture-design/products/item13',
+    imagePath: '/FurnitureDesign/item13.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 14,
+    url: '/furniture-design/products/item14',
+    imagePath: '/FurnitureDesign/item14.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 15,
+    url: '/furniture-design/products/item15',
+    imagePath: '/FurnitureDesign/item15.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+  {
+    id: 16,
+    url: '/furniture-design/products/item16',
+    imagePath: '/FurnitureDesign/item16.jpg',
+    title: 'プロダクトタイトルプロダクトタイトル',
+    price: '¥99,999 +tax',
+  },
+];
+
+export default products;
